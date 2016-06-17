b616e9f3049a5ff6e7ff96135f158984d522bd3f
Erixmanuel Hernandez <ericmanuel.hernandez@utp.ac.pa>
2016-06-17 18:31:40 -0500

    main.html delet /stylesheet

--- client/main.js
+++ client/main.js
@@ -1,14 +1,14 @@
 Template.navbar.events({
 	'click .js-show-login' : function(){
 		event.preventDefault();
-    let element = $('.js-container_signupbar');
-    if(element.hasClass('container_signupbar-show')){
-      element.removeClass('container_signupbar-show');
-      element.addClass('container_signupbar');
+    let element = $('.js-container_singupbar');
+    if(element.hasClass('container_singupbar-show')){
+      element.removeClass('container_singupbar-show');
+      element.addClass('container_singupbar');
     }
     else {
-      element.removeClass('container_signupbar');
-      element.addClass('container_signupbar-show');
+      element.removeClass('container_singupbar');
+      element.addClass('container_singupbar-show');
     }
 	}
 

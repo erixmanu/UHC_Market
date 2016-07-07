e57ce48fd8ea10d2820715ddf530012fe007334a
JosueU27 <urriolampudia@live.com>
2016-07-07 00:53:47 -0500

    Uso de Log In y SignUp

--- client/main.js
+++ client/main.js
@@ -10,10 +10,35 @@ Template.navbar.events({
     if(element.hasClass('container_signupbar-show')){
       element.removeClass('container_signupbar-show');
       element.addClass('container_signupbar');
-    }
+			document.getElementById('container_register').style.display = 'none';
+		}
     else {
       element.removeClass('container_signupbar');
       element.addClass('container_signupbar-show');
+			document.getElementById('container_register').style.display = 'block';
+
     }
+
+
+	}
+});
+
+Template.navbar.events({
+	'click .js-show-Ingresar' : function(){
+		event.preventDefault();
+    let element = $('.js-container_signupbar');
+    if(element.hasClass('container_signupbar-show')){
+      element.removeClass('container_signupbar-show');
+      element.addClass('container_signupbar');
+			document.getElementById('container_login').style.display = 'none';
+		}
+    else {
+      element.removeClass('container_signupbar');
+      element.addClass('container_signupbar-show');
+			document.getElementById('container_login').style.display = 'block';
+
+    }
+
+		
 	}
 });
